03b7968ee20d1e0c19333a64bcada2e5d73ad16f
san530san <alexmet04@gmail.com>
2021-05-16 22:17:10 +0700

    добавил подсказку команд и убрал лишний файл

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from aiogram import executor
 
 from loader import dp
 import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Уведомляет про запуск
-    await on_startup_notify(dispatcher)
-
+    # Устанавливаем дефолтные команды
+    await set_default_commands(dispatcher)
 
 if __name__ == '__main__':
-    # executor.start_polling(dp, on_startup=on_startup)
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=on_startup)
+    #executor.start_polling(dp)
